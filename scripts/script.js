447ebe69834f68f0cfd8f458b4625f837124cd39
alexsonalmeida <101877352+alexsonalmeida@users.noreply.github.com>
2023-01-15 17:34:33 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ scripts/script.js
@@ -0,0 +1,23 @@
+const projetosEscondidos = document.querySelectorAll('.projetoEscondido');
+const verMaisProjetosButton = document.querySelector('.verMaisProjetos');
+const linksProjetos = [
+    "https://github.com/VictorM-Coder/Goorg-Application",
+    "https://alexsonalmeida.github.io/calculadora/",
+    "https://alexsonalmeida.github.io/placar-de-truco/"
+]
+
+const mostrarProjetos = () => {
+    projetosEscondidos.forEach(projeto => {
+        projeto.classList.remove('projetoEscondido')
+    });
+
+    verMaisProjetosButton.parentNode.removeChild(verMaisProjetosButton);
+}
+
+const navegarParaIntroducao = () => {
+    location.href = '#introducao';
+}
+
+const navegarParaProjeto = (index) => {
+    window.open(linksProjetos[index]);
+}
---
+++ scripts/scrollReveal.js
@@ -0,0 +1,5 @@
+window.sr = ScrollReveal({reset: true})
+ScrollReveal({distance: '1rem'})
+
+ScrollReveal().reveal('.area1', {duration: 3000, origin: 'left'})
+ScrollReveal().reveal('.area2', {duration: 3000, origin: 'left'})
