27cd7e40388940ff7a2e939f6adfcdc538b9def4
Alexson Almeida <as3904527@gmail.com>
2023-01-15 14:44:18 -0300

    correcao nos arquivos

--- scripts/scrollReveal.js
+++ scripts/scrollReveal.js
@@ -1,5 +1,7 @@
-window.sr = ScrollReveal({reset: true})
-ScrollReveal({distance: '1rem'})
+window.sr = ScrollReveal({reset: true});
+ScrollReveal({distance: '1rem'});
 
-ScrollReveal().reveal('.area1', {duration: 3000, origin: 'left'})
-ScrollReveal().reveal('.area2', {duration: 3000, origin: 'left'})
+ScrollReveal().reveal('.area1', {duration: 3000, origin: 'left'});
+ScrollReveal().reveal('.area2', {duration: 3000,  origin: 'left'});
+ScrollReveal().reveal('.area3', {duration: 3000,  origin: 'left'});
+ScrollReveal().reveal('.area4', {duration: 3000,  origin: 'left'});
